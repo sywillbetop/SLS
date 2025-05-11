6b1e56d54ddeafae17695fa29e2141fca2cbc563
seosooyeon <seosooyeon@iplanbiz.co.kr>
2025-05-11 16:05:46 +0900

    #2 books.py building

---
+++ library.py
@@ -0,0 +1,9 @@
+'''
+3. library.py
+	3.1 하나의 클래스로 구성되며, 클래스 이름은 Library이다
+	3.2 Library 클래스는 도서관에 신간 책을 추가하는 add_books, 
+	오래된 책을 제거하는 remove_books, 대여 가능한 책들을 보여주는
+	show_books_available, 책을 대여하는 rent, 그리고 책을 반납하는
+	book_return 메소드들로 구성되며, 기타 다 필요하다고 생각되는 
+	기능들은 추가하기 바란다.
+'''
